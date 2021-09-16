a8bab4eb7941f64857255dafb18944e5580eae05
chengscc <44265641+xuchengcheng926@users.noreply.github.com>
2021-09-17 00:37:56 +0800

    Update flaypluse.sh

--- flaypluse.sh
+++ flaypluse.sh
@@ -1,9 +1,10 @@
 for ((i=0;i<$1;i++))
 do
+  port=$4+$i
   cd \
   && mkdir peer2profit$i && cd peer2profit$i \
   && wget -q https://raw.githubusercontent.com/xuchengcheng926/peer2profit/main/peer2fly.sh -O peer2fly.sh \
   && chmod +x peer2fly.sh \
   #&&./peer2fly.sh --email "$2" --number "10"  --socks "socks5 $3 $4+$i"  
-  echo  email "$2" "socks5 $3 $4+$i"
+  echo  email "$2" "socks5 $3 $port"
 done
