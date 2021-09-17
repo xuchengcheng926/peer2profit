56f08526c5518b100a6d02c146e5ff18cbe32dba
chengscc <44265641+xuchengcheng926@users.noreply.github.com>
2021-09-17 09:09:58 +0800

    Update goproxy.sh

--- goproxy.sh
+++ goproxy.sh
@@ -3,6 +3,6 @@ curl -L https://mirrors.host900.com/https://raw.githubusercontent.com/snail007/g
 for ((i=0;i<$1;i++))
 do
   port=$(($3+5000))
-  logdir="/proxylogs/"proxy$port".log"
+  logdir="proxylogs/"proxy$port".log"
   proxy socks -t tcp -p "0.0.0.0:$port" -T tcp -P "$2:$port"  --udp-port 0 --udp --forever --log $logdir --daemon
 done
